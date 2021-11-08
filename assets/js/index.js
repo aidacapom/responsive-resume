3cdf044ed82cbc9d4182e6ff813c3a83ca4b45f1
Aida Capó Martí <aidacapom@gmail.com>
2021-11-08 22:20:38 +0100

    Worked on pdf version styles and download functionality

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const generateBtn = document.getElementById('generate-pdf-btn')
+const pdfArea = document.getElementById('pdf-area')
+const pdfOptions = {
+    margin: 0, 
+    filename: 'aida-capo-resume.pdf',
+    image: { type: 'jpeg', quality: 0.98},
+    html2canvas: { scale: 4 }, 
+    jsPDF: { format: 'a4', orientation: 'portrait'}
+}
+
+generateBtn.addEventListener('click', () => {
+    scaleResume()
+    generatePDF()
+    setTimeout(removeScale, 5000)
+})
+
+function generatePDF() {
+    html2pdf(pdfArea, pdfOptions)
+}
+
+function scaleResume() {
+    document.body.classList.add('pdf')
+}
+
+function removeScale() {
+    document.body.classList.remove('pdf')
+}
+
