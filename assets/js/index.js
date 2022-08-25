15054276e688dbcddff6c0d4803e47fc2cc17762
Aida Capó Martí <36511149+ptaltavull@users.noreply.github.com>
2022-08-25 12:56:58 +0200

    pau

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ const generateBtn = document.getElementById('generate-pdf-btn')
 const pdfArea = document.getElementById('pdf-area')
 const pdfOptions = {
     margin: 0, 
-    filename: 'aida-capo-resume.pdf',
+    filename: 'resume.pdf',
     image: { type: 'jpeg', quality: 1},
     html2canvas: { scale: 4 }, 
     jsPDF: { format: 'a4', orientation: 'portrait'}
